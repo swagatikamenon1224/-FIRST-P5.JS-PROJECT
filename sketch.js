a637d1e6472a780c4146dd716c986cd94825eeab
swagatikamenon1224 <90810378+swagatikamenon1224@users.noreply.github.com>
2021-09-15 19:35:14 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(30, 30, 30);
+  circle(200, 30, 30);
+  circle(200, 160, 30);
+  circle(30, 160, 30);
+}
